5c0f74af66ee5da653a4b5b474fbb554b53b9648
Duck126 <jduck1133@gmail.com>
2018-05-01 13:56:53 -0500

    Initial File Structure and DB seed and schema files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger) VALUES ('Mushroom Swiss Burger', false);
+INSERT INTO burgers (burger) VALUES ('Bacon Avacado Burger', false);
+INSERT INTO burgers (burger) VALUES ('Habanero Burger', false);
